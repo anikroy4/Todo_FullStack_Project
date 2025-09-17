137755bc5c8462ba9f9207567d9e59a895b29c48
Anik Roy <anikroy.uiu@gmail.com>
2025-09-17 19:55:50 +0600

    update

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,32 @@
-let registrationController=( req, res)=>{
-    res.send("Registration Here");
+const jwt=require("jsonwebtoken");
+const bcryptjs=require("bcryptjs");
+const User=require("../model/userModel");
+
+
+
+const generateAccessToken=(user)=>{
+return jwt.sign({id:user._id},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"15m"})
+}
+const generateRefreshToken=(user)=>{
+return jwt.sign({id:user._id},process.env.REFRESH_TOKEN_SECRET,{expiresIn:"365d"})
+}
+
+
+
+
+let registrationController=async( req, res)=>{
+    const {username, email, password}=req.body;
+    const hashed=await bcryptjs.hash(password,10);
+
+    const user= new User({
+        username:username,
+        email : email,
+        password:String,
+        isVarified:false,
+    });
+    user.save();
+
+    res.send({message:" Registration successful. please check email"});
     
 }
 let loginController=(req, res)=>{
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -3,7 +3,7 @@ const { Schema }= mongoose;
 
 const userSchema= new Schema({
     username:String,
-    emal:{
+    email:{
         type:String,
         required:true,
         unique:true,
